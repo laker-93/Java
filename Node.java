814d49052a304eb482553d4700c03fc093fab851
Luke Purnell <luke_purnell@hotmail.com>
2016-05-23 22:29:43 +0100

    Added code to create a Node

--- Node.java
+++ Node.java
@@ -1,1 +1,29 @@
 //Creates a Node
+public class Node {
+	
+	int key;
+	String name;
+	boolean visited;
+
+	Node leftChild;
+	Node rightChild;
+
+	Node(int key, String name) {
+
+		this.key = key;
+		this.name = name;
+
+	}
+
+	public String toString() {
+
+		return name + " has the key " + key;
+    }
+	
+	public static void main(String[] args) {
+		
+		Node firstNode = new Node(0, "A");
+		String result = firstNode.toString();
+		System.out.println(result);
+	}
+}
